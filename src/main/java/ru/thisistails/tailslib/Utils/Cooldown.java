380195f521e1cc107ff3b718b0433d949ea6bf7c
ThIsIsTails <this.is.milestails@gmail.com>
2024-04-29 19:40:55 +0300

    Создания обобщённого класса Cooldown

---
+++ src/main/java/ru/thisistails/tailslib/Utils/Cooldown.java
@@ -0,0 +1,61 @@
+package ru.thisistails.tailslib.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import lombok.Getter;
+
+/**
+ * Простая реализация кулдауна для
+ * использования её в разных моментах.
+ * <p>Использует вызовы из {@link BukkitTask} из-за чего вызывать
+ * кд до момента загрузки сервера нельзя.</p>
+ * @see BukkitTask
+ * @see BukkitRunnable
+ */
+public class Cooldown<T> {
+
+    private @Getter Map<T, BukkitTask> cooldowns;
+    
+    public Cooldown() {
+        cooldowns = new HashMap<>();
+    }
+
+    /**
+     * Отправляет T в кулдаун.
+     * @param arg       Аргумент который отправлять в кд
+     * @param seconds   Секунды (уже умножены на 20 для тиков)
+     */
+    public void addNewCooldown(T arg, long seconds) {
+        seconds = seconds * 20;
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("TailsLib");
+
+        cooldowns.put(arg, new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                cooldowns.remove(arg);
+            }
+            
+        }.runTaskLater(plugin, seconds));
+    }
+
+    /**
+     * Убирает T из кулдауна.
+     * @param arg   Аргумент типа T который находится в списке.
+     */
+    public void removeCooldown(T arg) {
+        cooldowns.get(arg).cancel();
+        cooldowns.remove(arg);
+    }
+
+    public boolean containsCooldown(T arg) {
+        return cooldowns.containsKey(arg);
+    }
+
+}
